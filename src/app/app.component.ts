88d7238094c0bc7fb33197343e98bdfc400a9b5e
asungao <asungao@sina.com>
2017-05-12 11:24:42 +0800

    add comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  //Add comments
   inputHints = 'Please help to input something!'
+
 }
